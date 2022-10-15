64891dd3c43e02c51cec23f0c7ed0ada98a73cee
rkriar <42456883+rkriar@users.noreply.github.com>
2022-10-14 22:02:00 -0400

    adding files

---
+++ crypt1.pl
@@ -0,0 +1,37 @@
+% Enter the names of your group members below.
+% If you only have 2 group members, leave the last space blank
+%
+%%%%%
+%%%%% NAME: 
+%%%%% NAME:
+%%%%% NAME:
+%
+% Add the required atomic propositions and rules in the corresponding sections. 
+% If you put the propositions and rules in the wrong sections, you may lose marks.
+%
+% You may add additional comments as you choose but DO NOT MODIFY the comment lines below
+
+%%%%% ATOMIC
+% Put any atomic statements needed here
+
+
+
+%%%%% HELPER PREDICATES
+% Put any helper predicates here (leave empty if you do not have any
+
+
+
+%%%%% RULE: solve( ListOfVars )
+% Add the rules defining your solve program
+% MAKE SURE IT TAKES A LIST AS INPUT, SUCH THAT THAT LIST IS CONTAINS ALL VARIABLES
+
+
+
+%%%%% RULE: print_solution ( ListOfVars )
+%  Add rules the prints your solution in a readable format
+% MAKE SURE IT TAKES A LIST AS INPUT, SUCH THAT THAT LIST IS CONTAINS ALL VARIABLES
+
+
+
+%%%%% END
+% DO NOT PUT ANY ATOMIC PROPOSITIONS OR LINES BELOW
