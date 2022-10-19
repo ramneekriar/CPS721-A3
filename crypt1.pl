ed9e14e2aa8360c4b9b71d678b5750bff456873d
Karanvir Heer <karanvirheer@live.com>
2022-10-19 17:09:51 -0400

    Finished Q1

--- crypt1.pl
+++ crypt1.pl
@@ -14,42 +14,63 @@
 %%%%% ATOMIC
 % Put any atomic statements needed here
 
-
+dig(0). dig(1). dig(2). dig(3). dig(4). dig(5). dig(6). dig(7). dig(8). dig(9).
 
 %%%%% HELPER PREDICATES
-% Put any helper predicates here (leave empty if you do not have any
+% Put any helper predicates here (leave empty if you do not have any)
 
+all_diff([]).
+all_diff([H|T]) :- not member(H, T), all_diff(T).
 
+% member(Elem, [Elem|T]).
+% member(Elem, [H|T]) :- member(Elem, T).
 
 %%%%% RULE: solve( ListOfVars )
 % Add the rules defining your solve program
 % MAKE SURE IT TAKES A LIST AS INPUT, SUCH THAT THAT LIST IS CONTAINS ALL VARIABLES
 
-solve([S,T,R,A,W,O,C,H,I]) :- dig(S), dig(T), dig(R), dig(A), dig(W), dig(O), dig(C), dig(H), dig(I),
+solve([S,T,R,A,W,O,C,H,I]) :-
+    dig(S), dig(T), dig(R), dig(A), dig(W), dig(O), dig(C), dig(H), dig(I),
 
-S>0, T>0, W>0,
-S is (W*O) mod 10, C1 is (W*O) // 10,
-T is (A*O+C1) mod 10, C10 is (A*O+C1) //10,
-R is (R*O+C10) mod 10, C100 is (A*O+C10) //10,
-A is (T*O+C100) mod 10, C1000 is (A*O+C100) //10,
-W is (S*O+C1000) //10,
-I is (R+A+((T+W)//10)),
-H is (W+T+((A+R)//10)),
-C is (S+((W+T)//10)),
+    all_diff([S,T,R,A,W,O,C,H,I]),
 
-all_diff([S,T,R,A,W,O,C,H,I]).
+    S > 0, T > 0, W > 0, C > 0,
 
-dig(0). dig(1). dig(2). dig(3). dig(4). dig(5). dig(6). dig(7). dig(8). dig(9).
+    S is (O*W) mod 10, C1 is (O*W)//10,
+    T is (O*A+C1) mod 10, C10 is (O*A+C1)//10,
+    R is (O*R+C10) mod 10, C100 is (O*R+C10)//10,
+    A is (O*T+C100) mod 10, C1000 is (O*T+C100)//10,
+    W is (O*S+C1000) mod 10,
 
-all_diff([]).
-all_diff([N|L]) :- not member(N,L), all_diff(L).
+    W is (T*W) mod 10, CT1 is (T*W)//10,
+    A is (T*A+CT1) mod 10, CT10 is (T*A+CT1)//10,
+    R is (T*R+CT10) mod 10, CT100 is (T*R+CT10)//10,
+    T is (T*T+CT100) mod 10, CT1000 is (T*T+CT100)//10,
+    S is (T*S+CT1000) mod 10,
 
+    S is S,
+    R is (T+W) mod 10, CF1 is (T+W)//10,
+    I is (R+A+CF1) mod 10, CF10 is (R+A+CF1)//10,
+    A is (A+R+CF10) mod 10, CF100 is (A+R+CF10)//10,
+    H is (W+T+CF100) mod 10, CF1000 is (W+T+CF100)//10,
+    C is S + CF1000.
+
+% DOMAIN KB
 
 
 %%%%% RULE: print_solution ( ListOfVars )
 %  Add rules the prints your solution in a readable format
 % MAKE SURE IT TAKES A LIST AS INPUT, SUCH THAT THAT LIST IS CONTAINS ALL VARIABLES
-
+print_solution([S,T,R,A,W,O,C,H,I]) :-
+    solve([S,T,R,A,W,O,C,I]),
+    nl, write("S = "), write(S),
+    nl, write("T = "), write(T),
+    nl, write("R = "), write(R),
+    nl, write("A = "), write(A),
+    nl, write("W = "), write(W),
+    nl, write("O = "), write(O),
+    nl, write("C = "), write(C),
+    nl, write("H = "), write(I).
 
 
 %%%%% END
--- crypt2.pl
+++ crypt2.pl
@@ -14,23 +14,78 @@
 %%%%% ATOMIC
 % Put any atomic statements needed here
 
+dig(0). dig(1). dig(2). dig(3). dig(4). dig(5). dig(6). dig(7). dig(8). dig(9).
 
+car(0). car(1). car(3).
 
 %%%%% HELPER PREDICATES
-% Put any helper predicates here (leave empty if you do not have any
+% Put any helper predicates here (leave empty if you do not have any)
 
+all_diff([]).
+all_diff([H|T]) :- not member(H, T), all_diff(T).
 
 
 %%%%% RULE: solve( ListOfVars )
 % Add the rules defining your solve program
 % MAKE SURE IT TAKES A LIST AS INPUT, SUCH THAT THAT LIST IS CONTAINS ALL VARIABLES
 
+solve([S,T,R,A,W,O,C,H,I]) :-
+    
+    dig(S), S > 0, car(CF100),
+    C is S + CF100, C > 0,
+
+    dig(T), T>0, dig(W), W > 0,
+    H is (W+T+CF100) mod 10, CF1000 is (W+T+CF100)//10,
+
+    dig(O),
+    S is (O*W) mod 10, C1 is (O*W)//10,
+
+    car(CT100),
+    T is (T*T+CT100) mod 10, CT1000 is (T*T+CT100)//10,
+    S is (T*S+CT1000) mod 10,
+    S is S,
+
+    car(C100),
+    A is (O*T+C100) mod 10, C1000 is (O*T+C100)//10,
+
+    R is (T+W) mod 10, CF1 is (T+W)//10,
+
+    dig(R), car(CF10),
+    A is (A+R+CF10) mod 10, CF100 is (A+R+CF10)//10,
+
+    I is (R+A+CF1) mod 10, CF10 is (R+A+CF1)//10,
+
+    car(CT10),
+    R is (T*R+CT10) mod 10, CT100 is (T*R+CT10)//10,
+
+    W is (T*W) mod 10, CT1 is (T*W)//10,
+    A is (T*A+CT1) mod 10, CT10 is (T*A+CT1)//10,
+
+    car(C1000),
+    W is (O*S+C1000) mod 10,
+
+    T is (O*A+C1) mod 10, C10 is (O*A+C1)//10,
+
+    R is (O*R+C10) mod 10, C100 is (O*R+C10)//10,
+
+    all_diff([S,T,R,A,W,O,C,H,I]).
+
+
 
 
 %%%%% RULE: print_solution ( ListOfVars )
 %  Add rules the prints your solution in a readable format
 % MAKE SURE IT TAKES A LIST AS INPUT, SUCH THAT THAT LIST IS CONTAINS ALL VARIABLES
-
+print_solution([S,T,R,A,W,O,C,H,I]) :-
+    solve([S,T,R,A,W,O,C,I]),
+    nl, write("S = "), write(S),
+    nl, write("T = "), write(T),
+    nl, write("R = "), write(R),
+    nl, write("A = "), write(A),
+    nl, write("W = "), write(W),
+    nl, write("O = "), write(O),
+    nl, write("C = "), write(C),
+    nl, write("H = "), write(I).
 
 
 %%%%% END
