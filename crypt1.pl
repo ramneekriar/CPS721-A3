1bfcb713708e57c37ca2bd9cbfbe6ae65db8a7ce
Karanvir Heer <karanvirheer@live.com>
2022-10-23 20:08:47 -0400

    Added names to header

--- crypt1.pl
+++ crypt1.pl
@@ -2,9 +2,9 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
-%%%%% NAME:
-%%%%% NAME:
+%%%%% NAME: Karanvir Heer
+%%%%% NAME: Ramneek Riar
+%%%%% NAME: Shruti Sharma
 %
 % Add the required atomic propositions and rules in the corresponding sections. 
 % If you put the propositions and rules in the wrong sections, you may lose marks.
